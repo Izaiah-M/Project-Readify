8f1ca8758f9b33013762be85650f4841472ac8d6
iz.aia.h <imukisa024@gmail.com>
2023-03-15 17:24:51 +0300

    finished random book gen

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -29,6 +29,38 @@ const BookRequests = {
       console.log(error);
     }
   },
+
+  getRandomBook: async () => {
+    const categories = ["romance", "education", "fiction", "adventure"];
+
+    // To pick a random book categorie
+    const randomCategory =
+      categories[Math.floor(Math.random() * categories.length)];
+
+    // Url to fetch the books randomly from.
+    // https://www.googleapis.com/books/v1/volumes?q=subject:romance&key=AIzaSyCzwGl8yfmrxI2MSmEveV0YZhpIxjP2DOg
+
+    const fetchURL = `${baseURL}${endPoint}?q=subject:${randomCategory}&key=${APIKEY}`;
+
+    try {
+      const response = await axios.get(fetchURL);
+
+      const jsonResponse = await response.data.items;
+
+      const books = jsonResponse.map((book) => ({
+        id: book.accessInfo.id,
+        author: book.volumeInfo.authors,
+        category: book.volumeInfo.categories,
+        description: book.volumeInfo.description,
+        image: book.volumeInfo.imageLinks?.thumbnail,
+        title: book.volumeInfo.title,
+      }));
+
+      return books;
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 // console.log(await BookRequests.getBookBySearch("react"));
