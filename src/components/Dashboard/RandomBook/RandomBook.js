95053e40df36f96986a0914839c8994cff136b81
iz.aia.h <imukisa024@gmail.com>
2023-03-15 19:54:35 +0300

    added small style to the random book cards

--- src/components/Dashboard/RandomBook/RandomBook.js
+++ src/components/Dashboard/RandomBook/RandomBook.js
@@ -2,15 +2,18 @@ import "./RandomBook.css";
 
 export const RandomBook = ({ books }) => {
   return (
-    <div>
-      {books.map((book, index) => {
-        return (
-          <div key={index}>
-            <img src={book.image} alt={book.title} />
-            <p>{book.title}</p>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <p>Reading Ideas </p>
+      <div className="randbook-container">
+        {books.map((book, index) => {
+          return (
+            <div key={index} className="randbook-card">
+              <img src={book.image} alt={book.title} />
+              {/* <p>{book.title}</p> */}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
