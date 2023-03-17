b930a1ca0735e44af8138a7b427c751f4f50ef62
iz.aia.h <imukisa024@gmail.com>
2023-03-17 15:20:34 +0300

    almost done with it

--- src/components/Dashboard/InfoCenter/InfoCenter.js
+++ src/components/Dashboard/InfoCenter/InfoCenter.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import "./InfoCenter.css";
 
 export const InfoCenter = () => {
@@ -26,6 +29,18 @@ export const InfoCenter = () => {
             setNewEvent({ ...newEvent, title: target.value })
           }
         />
+        <ReactDatePicker
+          placeholderText="Start Date"
+          selected={newEvent.start}
+          onChange={(start) => setNewEvent({ ...newEvent, start })}
+        />
+
+        <ReactDatePicker
+          placeholderText="End Date"
+          selected={newEvent.end}
+          onChange={(end) => setNewEvent({ ...newEvent, end })}
+        />
+        <button onClick={handleAddEvent}>Add event</button>
       </div>
     </div>
   );
