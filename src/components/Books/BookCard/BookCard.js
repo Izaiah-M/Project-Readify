7e79f5e7feac29e980dcdff58b8572d7a2a7397a
iz.aia.h <imukisa024@gmail.com>
2023-03-22 08:03:10 +0300

    added cancel button and form for adding book to schedule

--- src/components/Books/BookCard/BookCard.js
+++ src/components/Books/BookCard/BookCard.js
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { BookDetails } from "../BookDetails/BookDetails";
 import "./BookCard.css";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 import { BsXLg } from "react-icons/bs";
 
 export const BookCard = ({ image, title, description, author, category }) => {
   const [selectedBook, setSelectedBook] = useState(null);
+  const [clickSchedule, setClickSchedule] = useState(false);
 
   const handleBookClick = () => {
     setSelectedBook(title);
@@ -15,7 +18,15 @@ export const BookCard = ({ image, title, description, author, category }) => {
     setSelectedBook(null);
   };
 
-  const handleAddSchedule = () => {};
+  const handleAddSchedule = () => {
+    setClickSchedule(true);
+    console.log("clicked");
+  };
+
+  const handleCancel = () => {
+    setClickSchedule(false);
+    console.log("close");
+  };
 
   return (
     <>
@@ -24,7 +35,7 @@ export const BookCard = ({ image, title, description, author, category }) => {
         <p className="p">{title}</p>
       </div>
       {selectedBook && (
-        <div className="modal-overlay" onClick={handleClose}>
+        <div className="modal-overlay">
           <div className="modal">
             <BsXLg onClick={handleClose} className="close" />
             <BookDetails
@@ -35,7 +46,40 @@ export const BookCard = ({ image, title, description, author, category }) => {
               author={author}
               category={category}
             />
-            <button className="add-schedule">Add to schedule</button>
+            <button className="add-schedule" onClick={handleAddSchedule}>
+              Add to schedule
+            </button>
+            <div className="scheduleForm">
+              {clickSchedule && (
+                <div>
+                  <form>
+                    <input type="text" required placeholder="Add Title" />
+
+                    <div className="selectday">
+                      <ReactDatePicker
+                        className="start-date"
+                        required
+                        placeholderText="Start Date"
+                      />
+
+                      <ReactDatePicker
+                        className="end-date"
+                        required
+                        placeholderText="End Date"
+                      />
+                    </div>
+                    <input
+                      type="submit"
+                      value="Add Event"
+                      className="submit-btn"
+                    />
+                  </form>
+                  <button className="add-schedule" onClick={handleCancel}>
+                    Cancel
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       )}
