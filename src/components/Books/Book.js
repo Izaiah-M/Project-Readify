2efa9d05b24fb3a93050d3c011f5d26251514c09
iz.aia.h <imukisa024@gmail.com>
2023-03-23 07:41:03 +0300

    fixed fetch problem

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -4,28 +4,28 @@ import { BookCard } from "./BookCard/BookCard";
 import "./Book.css";
 
 export const Book = ({ data, handleSubmit, newEvent, setNewEvent }) => {
+  if (!data) {
+    return <p>Oops, something went wrong :{"("}.</p>;
+  }
+
   return (
     <div className="Book">
-      {data !== null ? (
-        data.map((book, index) => {
-          return (
-            <div key={index + 1} className="Card">
-              <BookCard
-                image={book.image}
-                title={book.title}
-                description={book.description}
-                author={book.author}
-                category={book.category}
-                handleSubmit={handleSubmit}
-                newEvent={newEvent}
-                setNewEvent={setNewEvent}
-              />
-            </div>
-          );
-        })
-      ) : (
-        <p>:{"("} Oops something went wrong</p>
-      )}
+      {data.map((book, index) => {
+        return (
+          <div key={index + 1} className="Card">
+            <BookCard
+              image={book.image}
+              title={book.title}
+              description={book.description}
+              author={book.author}
+              category={book.category}
+              handleSubmit={handleSubmit}
+              newEvent={newEvent}
+              setNewEvent={setNewEvent}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
